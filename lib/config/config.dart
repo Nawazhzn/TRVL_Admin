e0cd698edc6866a4d715459b35ee4bd1f174b659
Nawaz Hasan <nawazhasan34@gmail.com>
2023-05-17 15:18:47 +0530

    initial commit

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  final String appName = 'TRVL'; //app name
+  final String testerPassword = 'test12345'; //testing password
+
+  //firebase server token for push notication
+  final String serverToken =
+      'AAAA58Jq1rU:APA91bEfpZdRH0sYsq0dJ_sRAVv7gru3COQLWSrh9fV1GASHHQ_-AQxWMZkhVpJP5uQDY9_64-FWzZ4kJvMQLkJ3HkVVVXXsI7wLN1324Q8Bni66iZwpudZJfmSslhv-iS1w2NXWeSft';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
---
+++ lib/pages/settings.dart
@@ -0,0 +1,35 @@
+import 'package:admin/blocs/admin_bloc.dart';
+import 'package:admin/utils/dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Settings extends StatelessWidget {
+  const Settings({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final AdminBloc ab = Provider.of<AdminBloc>(context, listen: false);
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      SizedBox(
+        height: MediaQuery.of(context).size.height * 0.05,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'Settings',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
+          ),
+        ],
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 5, bottom: 50),
+        height: 3,
+        width: 50,
+        decoration: BoxDecoration(
+            color: Colors.indigoAccent,
+            borderRadius: BorderRadius.circular(15)),
+      ),
+    ]);
+  }
+}
